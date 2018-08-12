937a6f0f9f8c2f6d36ba387651fccb827d9b8a27
Jubair Hossain <jubair@mpower-social.com>
2018-08-12 17:42:36 +0600

    Cattle Profile Style

--- kobocat/onadata/apps/livestock/urls.py
+++ kobocat/onadata/apps/livestock/urls.py
@@ -46,7 +46,7 @@ urlpatterns = patterns('',
                        url(r'^getAdvisoryData/$', views.getAdvisoryData, name='getAdvisoryData'),
                        url(r'^sickness_list/$', views.sickness_list, name='sickness_list'),
                        url(r'^getSicknessData/$', views.getSicknessData, name='getSicknessData'),
-url(r'^get_clinical_findings/$', views.get_clinical_findings, name='get_clinical_findings'),
+                    url(r'^get_clinical_findings/$', views.get_clinical_findings, name='get_clinical_findings'),
 
 
                        #******************   MOBILE API     ***********************************#
